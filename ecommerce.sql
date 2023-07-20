d1ebe95101cd331bc1f9941b4eeb3f6a3bc4f78e
chandru-2023 <chandrusivanesan@gmail.com>
2023-07-20 23:19:10 +0000

    total number of products sold in february 2019

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,5 +1,8 @@
-select distinct(purchase_date) as purchasedate
+select *
 from ecommerce;
 
-select distinct(COUNTRY) as country
-from ecommerce;
+desc ecommerce
+
+select count(PRODUCTNO) AS TOTAL_PRODUCTS
+from ECOMMERCE
+where PURCHASE_DATE between '01-02-2019' and '28-02-2019';
