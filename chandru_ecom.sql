cb7c6bd7bbc776cbf7a1b2299fc766f378539f8c
chandru-2023 <chandrusivanesan@gmail.com>
2023-07-21 02:10:28 +0000

    count the customers from each country

--- chandru_ecom.sql
+++ chandru_ecom.sql
@@ -13,7 +13,15 @@ from ECOMMERCE
 GROUP BY substr(PURCHASE_DATE, 1,4);
 
 /*  total sales amount of each product on a month-wise basis in the year 2019   */
+
 set timing on;
 SELECT  productname, SUM(price*quantity) AS total_sales_amount,substr(PURCHASE_DATE, 6,2) AS month 
 FROM ECOMMERCE 
 WHERE substr(PURCHASE_DATE, 1,4) = '2019' GROUP BY substr(PURCHASE_DATE, 6,2), productname;
+
+/*  count the customers from each country   */
+
+set timing on;
+select count(CUSTOMERNO) as total_customers,COUNTRY
+from ECOMMERCE
+group by country;
