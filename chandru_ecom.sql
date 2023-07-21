8a1351f0c0f698b397fb2b5b7bc561c1face4067
chandru-2023 <chandrusivanesan@gmail.com>
2023-07-21 02:01:22 +0000

    total sales of each product on a month-wise

--- chandru_ecom.sql
+++ chandru_ecom.sql
@@ -12,3 +12,8 @@ select substr(PURCHASE_DATE, 1,4) AS year, sum(price) as total_sales_amount
 from ECOMMERCE
 GROUP BY substr(PURCHASE_DATE, 1,4);
 
+/*  total sales amount of each product on a month-wise basis in the year 2019   */
+set timing on;
+SELECT  productname, SUM(price*quantity) AS total_sales_amount,substr(PURCHASE_DATE, 6,2) AS month 
+FROM ECOMMERCE 
+WHERE substr(PURCHASE_DATE, 1,4) = '2019' GROUP BY substr(PURCHASE_DATE, 6,2), productname;
