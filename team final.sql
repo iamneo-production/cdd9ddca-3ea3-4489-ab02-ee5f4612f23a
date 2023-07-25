f9a6e672d7b5a1f987055f0ef04e02d10793adc4
Mani7991 <20955A0507@iare.ac.in>
2023-07-25 15:55:48 +0000

    Backup Commit

--- team final.sql
+++ team final.sql
@@ -1,4 +1,4 @@
---------------------Manikanta-----------------------
+---------------------------------------Manikanta-------------------------------------------------------------
 
 set timing on;
 
@@ -30,53 +30,8 @@ group by country order by CUSTOMER_COUNT desc;
 
 SELECT SUBSTR(PURCHASE_DATE,1,4) AS YEAR,PRODUCTNAME FROM ECOMMERCE GROUP BY SUBSTR(PURCHASE_DATE,1,4),PRODUCTNAME;
 
---EXTRA QUERIES
 
---6.Write a sql query to count the customers from each year?--
-
-select count(CUSTOMERNO) as customers_count,substr(purchase_date,1,4) as year from ecommerce 
-group by substr(purchase_date,1,4);
-
---7.Write a sql query to find total sale amount of each product in the year 2019?--
-
-select sum(price*quantity) as Amount,PRODUCTNAME,purchase_date from ecommerce where substr(purchase_date,1,4)='2019'
-group by substr(purchase_date,1,4), PRODUCTNAME,purchase_date;
-
---8.Write a sql query to display the order details of who are all ordered from country Australia?--
-
-select * from ecommerce where country='"Australia"';
-
---9.Write a sql query to display the country name in ascending order?--
-
-select distinct(Country) from ecommerce order by country ASC;
-
---10.Write a sql query to display the product list the price is above 2.01?--
-
-select PRODUCTNAME,PRICE from ecommerce where price>2.01 order by price asc;
-
---11.Write a sql query to display all unique products in descending order?--
-
-select distinct(PRODUCTNAME) from ECOMMERCE order by productname desc;
-
---12.Write a sql query to find which product is maximum purchased?--
-
-select max(PRODUCTNAME) from ecommerce;
-
---13.Write a sql query to find which product is minimum purchased?--
-
-select min(PRODUCTNAME) from ECOMMERCE;
-
---14.Write a sql Query to find Average of the Products?--
-
-select Avg(price) as Average from ecommerce;
-
---15.Write a sql query to find total number of Products?--
-
-select count(PRODUCTNAME) as Total_Products from ecommerce;
-
-
------------------Niharika----------------------------------------------------------------
-
+---------------------------------------------Niharika----------------------------------------------------------------
 
 set timing on;
 
@@ -99,58 +54,7 @@ select count(CUSTOMERNO) total_customers from ECOMMERCE group by COUNTRY;
 select distinct(PRODUCTNAME), substr(PURCHASE_DATE,1,4) yearly_basis from ECOMMERCE group by substr(PURCHASE_DATE,1,4), PRODUCTNAME;
 
 
-
-/*-----------------Additional queries------------------*/
-/*1.Find all transactions made on a particular date*/
-SELECT * FROM ecommerce WHERE purchase_date = '2019-02-02';
-
-/*2.Get the total number of transactions*/
-SELECT COUNT(*) AS TotalTransactions FROM ecommerce;
-
-/*3.transactions with a price greater than a specific value*/
-SELECT * FROM ecommerce WHERE Price>=1.94;
-
-/*4.the total revenue generated from all transactions*/
-SELECT SUM(Price * Quantity) AS TotalRevenue FROM ecommerce;
-
-/*5.the transactions with a quantity greater than a specific value*/
-SELECT * FROM ecommerce WHERE Quantity >12;
-
-/*6.Retrieve the latest transaction made*/
-SELECT * FROM ecommerce ORDER BY purchase_date DESC
-FETCH FIRST 1 ROW ONLY;
-
-/*7.Get the average price of all transactions*/
-SELECT AVG(Price) AS AveragePrice FROM ecommerce;
-
-/*8.Find the transactions with a specific transaction number and customer number*/
-SELECT * FROM ecommerce WHERE Transactionno = '536384'
-  AND Customerno = 16029;
-
-/*9.display table ecommerce*/
-select * from ecommerce;
-
-/*10.display productname in desc order*/
-select productname from ecommerce order by productname desc;
-
-/*11.display productname in alphabetical order*/
-select productname from ecommerce order by productname;
-
-/*12. max productname */
-select max(PRODUCTNAME) from ECOMMERCE;
-
-/*13.display unique customerno*/
-select distinct(CUSTOMERNO) from ECOMMERCE;
-
-/*14. Between operator */
-select PRODUCTNAME from ECOMMERCE where PRICE between 1.88 and 2.14;
-
-/* 15.IN operator*/
-SELECT * from ECOMMERCE where QUANTITY in (2,4,6);
-
-
-
----------------------------Neeraj chekuri----------------------------------------
+--------------------------------------Neeraj chekuri--------------------------------------------------
 
 /*
 sql query to find how many products were sold
@@ -196,7 +100,7 @@ select substr(purchase_date,1,4) as years,productname from ecommerce group by su
 
 
 
-------------------------Susmitha-------------------------------------------------------------------------------------------------------
+------------------------------------Susmitha-------------------------------------------------------------------------------------------------------
 
 -- BELOW QUERIES ARE GIVEN AS TASKS TO DO IN WORKSPACE.
 
@@ -221,91 +125,8 @@ select country,count(CUSTOMERNO) as total_count from ECOMMERCE group by COUNTRY;
 select substr(purchase_date,1,4) as year, PRODUCTNAME  from ECOMMERCE  group by substr(purchase_date,1,4),PRODUCTNAME;
 
 
---I HAVE TRIED BELOW QUERIES WHICH ARE NOT MENTIONED IN WORKSPACE 
 
-
--- 6. Get the earliest transaction date in the table.
-
-SELECT MIN(PURCHASE_DATE) AS EARLIEST_DATE FROM ECOMMERCE;
-
--- 7.Retrieve transactions where the quantity is less than 10.
-
-SELECT * FROM ECOMMERCE WHERE QUANTITY < 10;
-
--- 8.Get the most expensive product sold.
-
-SELECT DISTINCT(PRODUCTNAME) FROM ECOMMERCE WHERE PRICE = (SELECT MAX(PRICE) FROM ECOMMERCE);
-
--- 9.Get the  products with the sales revenue IN DECREASING order.
-
-SELECT PRODUCTNO, SUM(PRICE * QUANTITY) AS TOTAL_REVENUE FROM ECOMMERCE GROUP BY PRODUCTNO ORDER BY TOTAL_REVENUE ;
-
--- 10.Get the average price of products sold.
-
-SELECT AVG(PRICE) AS AVERAGE_PRICE FROM ECOMMERCE;
-
--- 11.Get the maximum quantity sold in a single transaction.
-
-SELECT MAX(QUANTITY) AS MAX_QUANTITY_SOLD FROM ECOMMERCE;
-
--- 12. Retrieve transactions where the customer number is null.
-
-SELECT * FROM ECOMMERCE WHERE CUSTOMERNO IS NULL;
-
--- 13.Get the total number of transactions.
-
-SELECT COUNT(*) AS TOTAL_TRANSACTIONS FROM ECOMMERCE;
-
--- 14.Retrieve transactions where the price is greater than 100.
-
-SELECT * FROM ECOMMERCE WHERE PRICE > 100;
-
--- 15.Get the Latest transaction date in the table.
-
-SELECT MAX(PURCHASE_DATE) AS LATEST_DATE FROM ECOMMERCE;
-
--- 16.Get the total quantity sold for each product.
-
-SELECT PRODUCTNO, SUM(QUANTITY) AS TOTAL_QUANTITY FROM ECOMMERCE GROUP BY PRODUCTNO;
-
--- 17.Get the total sales revenue for transactions in a specific country.
-
-SELECT SUM(PRICE * QUANTITY) AS TOTAL_REVENUE FROM ECOMMERCE WHERE COUNTRY = '"United Kingdom"';
-
--- 18.Retrieve transactions where the product name starts with "A"
-
-SELECT * FROM ECOMMERCE WHERE PRODUCTNAME LIKE '"A%';
-
--- 19.Retrieve transactions where the product name ends with "book".
-
-SELECT * FROM ECOMMERCE WHERE PRODUCTNAME LIKE '%book"';
-
---20. Retrieve transactions where the product name contains "Holder".
-
-SELECT * FROM ECOMMERCE WHERE PRODUCTNAME LIKE '%Holder%';
-
---21. Retrieve transactions with a price greater than the average price.
-
-SELECT * FROM ECOMMERCE WHERE PRICE > (SELECT AVG(PRICE) FROM ECOMMERCE);
-
---22. Retrieve transactions where the quantity is not equal to 0.
-
-SELECT * FROM ECOMMERCE WHERE QUANTITY <> 0;
-
---23. Retrieve transactions where the country is either "United States" or "Canada".
-
-SELECT * FROM ECOMMERCE WHERE COUNTRY IN ('"United States"', '"Canada"');
-
---24. Retrieve transactions where the quantity sold is the same as the price.
-
-SELECT * FROM ECOMMERCE WHERE QUANTITY = PRICE;
-
---25. Retrieve transactions where the price is a whole number (no decimal places).
-
-SELECT * FROM ECOMMERCE WHERE PRICE = ROUND(PRICE);
-
-
-------------------------------------Saikumar Bodhas--------------------------------------------
+---------------------------------------------------Saikumar Bodhas------------------------------------------------
 
 
 --SQL QUERY TO KNOW HOW MANY PRODUCTS WERE SOLD IN FEBRFEBRUARY 2019
@@ -376,6 +197,77 @@ from ECOMMERCE;
 
 ------------------------------------------Eswar--------------------------------------------------------------------
 
+
+SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
+
+EXPLAIN PLAN FOR SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+SELECT substr(purchase_date,1,4),SUM(PRICE * QUANTITY) FROM ECOMMERCE GROUP BY substr(purchase_date,1,4);
+CREATE INDEX 
+EXPLAIN PLAN FOR   
+SELECT substr(purchase_date,1,4),SUM(PRICE) FROM ECOMMERCE GROUP BY substr(purchase_date,1,4);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+
+SELECT substr(purchase_date,6,2),SUM(PRICE * QUANTITY) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
+GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
+EXPLAIN PLAN FOR SELECT substr(purchase_date,6,2),SUM(PRICE) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
+GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+SELECT COUNTRY,COUNT(*) FROM ECOMMERCE GROUP BY COUNTRY;
+EXPLAIN PLAN FOR 
+SELECT COUNTRY,COUNT(*) FROM ECOMMERCE GROUP BY COUNTRY;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
+GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
+CREATE INDEX PRODUCTNAME_PURCHASE_DATE_IDX ON ECOMMERCE(PRODUCTNAME,PURCHASE_DATE);
+EXPLAIN PLAN FOR
+SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
+GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY()); 
+
+
+
+
 ------------------------------------------Birudula Devika-----------------------------------------------------------------------
 
+
+select count(*) as products_sold
+from ecommerce where substr(purchase_date,1,4)='2019' and
+substr(purchase_date,6,2)='02';
+
+select substr(purchase_date,1,4) as years, sum(price*quantity) as total_sale_amount
+from ecommerce
+group by substr(purchase_date,1,4);
+
+select substr(purchase_date,6,2) as months,productname, sum(price*quantity) as total_sale_amount
+from ecommerce where substr(purchase_date,1,4)='2019'
+group by substr(purchase_date,6,2), productname;
+
+select country,count(*) as no_of_customers from ecommerce
+group by country;
+
+select substr(purchase_date,1,4) as years, productname
+from ecommerce
+group by substr(purchase_date,1,4),productname;
+
+
+
 ------------------------------------------Varaprasad---------------------------------------------------------------------------
+
+
+select count(*) as products_sold from ecommerce where substr(purchase_date,1,4)='2019' and 
+substr(purchase_date,6,2)='02';
+
+select substr(purchase_date,1,4) as years,sum(price*quantity) as total_sale_amount from ecommerce 
+group by substr(purchase_date,1,4);
+
+select substr(purchase_date,6,2) as months,productname,sum(price*quantity) as total_sale_amount from ecommerce 
+where substr(purchase_date,1,4)='2019' group by substr(purchase_date,6,2), productname;
+
+select country,count(*) as no_of_customers from ecommerce group by country;
+
+select substr(purchase_date,1,4) as years,productname from ecommerce group by substr(purchase_date,1,4), productname;
