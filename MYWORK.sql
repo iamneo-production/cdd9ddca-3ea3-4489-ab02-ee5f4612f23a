44cfb4874a1a118f79142f4a81134dcdb24a4fe2
eswarchivatam <eswarchivatam@gmail.com>
2023-07-25 16:28:33 +0000

    MODIFIED

--- MYWORK.sql
+++ MYWORK.sql
@@ -15,7 +15,8 @@ SELECT * FROM ECOMMERCE;
 
 */
 
-
+/*
+SET TIMING ON ;
 SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
 
 EXPLAIN PLAN FOR SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
@@ -46,3 +47,19 @@ EXPLAIN PLAN FOR
 SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
 GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
 SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY()); 
+*/
+
+SET TIMING ON ;
+SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
+
+
+SELECT substr(purchase_date,1,4),SUM(PRICE * QUANTITY) FROM ECOMMERCE GROUP BY substr(purchase_date,1,4);
+
+
+SELECT substr(purchase_date,6,2),SUM(PRICE * QUANTITY) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
+GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
+
+SELECT COUNTRY,COUNT(*) FROM ECOMMERCE GROUP BY COUNTRY;
+
+SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
+GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
