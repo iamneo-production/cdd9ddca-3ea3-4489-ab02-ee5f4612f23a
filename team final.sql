862939e5b3750a6002abce5f9ff131b2d9e49d76
Mani7991 <20955A0507@iare.ac.in>
2023-07-26 06:25:13 +0000

    team queries

--- team final.sql
+++ team final.sql
@@ -1,3 +1,33 @@
+-----------------------------------------OPTIMIZED QUERIES------------------------------------------------------------------
+
+set timing on;
+
+--1.Write a sql query to how many products were sold in February 2019?--
+
+select count(PRODUCTNO) as products_sold_FEB2019 from ECOMMERCE where PURCHASE_DATE like '2019-02-%';
+
+/* 2.total sales amount in each year*/
+
+select sum(PRICE*QUANTITY) as total_salesamount from ECOMMERCE group by substr(PURCHASE_DATE,1,4);
+select sum(PRICE*QUANTITY) as total_salesamount, substr(PURCHASE_DATE,1,4) Yearly from ECOMMERCE group by substr(PURCHASE_DATE,1,4);
+
+--3.Write a sql Query to what was the total sales amount of each product on a month-wise basis in the year 2019?--
+
+SELECT substr(purchase_date,6,2),SUM(PRICE * QUANTITY) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
+GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
+
+--4.write a sql query to count the customers from each country?--
+
+select country,count(*) as no_of_customers from ecommerce group by country;
+
+--5.Write a sql query to List all the unique product names sold from each year?--
+
+SELECT EXTRACT(YEAR FROM TO_DATE(PURCHASE_DATE,'YYYY-MM-DD')) AS SALES_YEAR,
+PRODUCTNAME AS PRODUCT_NAME
+FROM ECOMMERCE 
+GROUP BY EXTRACT(YEAR FROM TO_DATE(PURCHASE_DATE,'YYYY-MM-DD')),PRODUCTNAME;
+
+
 ---------------------------------------Manikanta-------------------------------------------------------------
 
 set timing on;
@@ -198,48 +228,30 @@ from ECOMMERCE;
 ------------------------------------------Eswar--------------------------------------------------------------------
 
 
+SET TIMING ON ;
 SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
 
-EXPLAIN PLAN FOR SELECT COUNT(*) FROM ECOMMERCE WHERE substr(purchase_date,6,2) =02 AND substr(purchase_date,1,4) =2019;
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
 
 SELECT substr(purchase_date,1,4),SUM(PRICE * QUANTITY) FROM ECOMMERCE GROUP BY substr(purchase_date,1,4);
-CREATE INDEX 
-EXPLAIN PLAN FOR   
-SELECT substr(purchase_date,1,4),SUM(PRICE) FROM ECOMMERCE GROUP BY substr(purchase_date,1,4);
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
 
 
 SELECT substr(purchase_date,6,2),SUM(PRICE * QUANTITY) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
 GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
-EXPLAIN PLAN FOR SELECT substr(purchase_date,6,2),SUM(PRICE) FROM ECOMMERCE  WHERE substr(purchase_date,1,4)=2019
-GROUP BY substr(purchase_date,6,2) ORDER BY substr(purchase_date,6,2);
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
 
 SELECT COUNTRY,COUNT(*) FROM ECOMMERCE GROUP BY COUNTRY;
-EXPLAIN PLAN FOR 
-SELECT COUNTRY,COUNT(*) FROM ECOMMERCE GROUP BY COUNTRY;
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
 
 SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
 GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
-CREATE INDEX PRODUCTNAME_PURCHASE_DATE_IDX ON ECOMMERCE(PRODUCTNAME,PURCHASE_DATE);
-EXPLAIN PLAN FOR
-SELECT DISTINCT PRODUCTNAME,substr(purchase_date,1,4) AS YEAR FROM ECOMMERCE 
-GROUP BY PRODUCTNAME,substr(purchase_date,1,4) ORDER BY substr(purchase_date,1,4);
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY()); 
-
 
 
 
 ------------------------------------------Birudula Devika-----------------------------------------------------------------------
 
-
-select count(*) as products_sold
+select count(*) products_sold
 from ecommerce where substr(purchase_date,1,4)='2019' and
 substr(purchase_date,6,2)='02';
 
-select substr(purchase_date,1,4) as years, sum(price*quantity) as total_sale_amount
+select substr(purchase_date,1,4) as years,sum(price*quantity) as total_sale_amount
 from ecommerce
 group by substr(purchase_date,1,4);
 
@@ -253,7 +265,6 @@ group by country;
 select substr(purchase_date,1,4) as years, productname
 from ecommerce
 group by substr(purchase_date,1,4),productname;
-
 
 
 ------------------------------------------Varaprasad---------------------------------------------------------------------------
