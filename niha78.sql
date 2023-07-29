dd523a0dd790bb659ad5d0831b9eeaf437a751da
niha78 <19951a1256@iare.ac.in>
2023-07-29 08:00:37 +0000

    commited change

--- niha78.sql
+++ niha78.sql
@@ -22,48 +22,42 @@ select distinct(PRODUCTNAME), substr(PURCHASE_DATE,1,4) yearly_basis from ECOMME
 
                     --Additional queries--
 --1.Find all transactions made on a particular date--
-SELECT * FROM ecommerce WHERE purchase_date = '2019-02-02';
+SELECT productname FROM ecommerce WHERE purchase_date = '2019-02-02';
 
 --2.Get the total number of transactions--
 SELECT COUNT(*) AS TotalTransactions FROM ecommerce;
 
 --3.transactions with a price greater than a specific value--
-SELECT * FROM ecommerce WHERE Price>=1.94;
+SELECT price FROM ecommerce WHERE Price>=1.94;
 
 --4.the total revenue generated from all transactions--
 SELECT SUM(Price * Quantity) AS TotalRevenue FROM ecommerce;
 
 --5.the transactions with a quantity greater than a specific value--
-SELECT * FROM ecommerce WHERE Quantity >12;
+SELECT productname FROM ecommerce WHERE Quantity >12;
 
 --6.Retrieve the latest transaction made--
-SELECT * FROM ecommerce ORDER BY purchase_date DESC
+SELECT productname FROM ecommerce ORDER BY purchase_date DESC
 FETCH FIRST 1 ROW ONLY;
 
 --7.Get the average price of all transactions--
 SELECT AVG(Price) AS AveragePrice FROM ecommerce;
 
 --8.Find the transactions with a specific transaction number and customer number--
-SELECT * FROM ecommerce WHERE Transactionno = '536384'
+SELECT productname FROM ecommerce WHERE Transactionno = '536384'
   AND Customerno = 16029;
 
---9.display table ecommerce--
-select * from ecommerce;
-
---10.display productname in desc order--
+--9.display productname in desc order--
 select productname from ecommerce order by productname desc;
 
---11.display productname in alphabetical order--
+--10.display productname in alphabetical order--
 select productname from ecommerce order by productname;
 
---12. max productname--
+--11. max productname--
 select max(PRODUCTNAME) from ECOMMERCE;
 
---13.display unique customerno--
+--12.display unique customerno--
 select distinct(CUSTOMERNO) from ECOMMERCE;
 
---14. Between operator --
+--13. Between operator --
 select PRODUCTNAME from ECOMMERCE where PRICE between 1.88 and 2.14;
-
--- 15.IN operator--
-SELECT * from ECOMMERCE where QUANTITY in (2,4,6);
