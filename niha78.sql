2aa50eb954bcdd30948f9da96d4c107aa5cccda6
niha78 <19951a1256@iare.ac.in>
2023-07-30 17:46:52 +0000

    finalcommit1

--- niha78.sql
+++ niha78.sql
@@ -1,26 +1,26 @@
 set timing on;
 
 -- 1.how many products were sold in feb 2019--
-select count(PRODUCTNAME) sold_products from ECOMMERCE where PURCHASE_DATE like '2019-02-%';
+select count(PRODUCTNAME) as sold_products from ECOMMERCE where PURCHASE_DATE like '2019-02-%';
 
 -- 2.total sales amount in each year--
 select sum(PRICE*QUANTITY) as total_salesamount from ECOMMERCE group by substr(PURCHASE_DATE,1,4);
 select sum(PRICE*QUANTITY) as total_salesamount, substr(PURCHASE_DATE,1,4) Yearly from ECOMMERCE group by substr(PURCHASE_DATE,1,4);
 
 -- 3.total sales amount of each product on month basis in year 2019--
-select PRODUCTNAME, sum(PRICE*QUANTITY) total_salesamount, substr(PURCHASE_DATE,6,2) monthwise from ECOMMERCE where PURCHASE_DATE like '2019-%' group by PRODUCTNAME, substr(PURCHASE_DATE,6,2); 
-select sum(PRICE*QUANTITY) total_salesamount from ECOMMERCE where PURCHASE_DATE like '2019-%' group by PRODUCTNAME, substr(PURCHASE_DATE,6,2);
+select PRODUCTNAME, sum(PRICE*QUANTITY) as total_salesamount, substr(PURCHASE_DATE,6,2) monthwise from ECOMMERCE where PURCHASE_DATE like '2019-%' group by PRODUCTNAME, substr(PURCHASE_DATE,6,2); 
+select sum(PRICE*QUANTITY) as total_salesamount from ECOMMERCE where PURCHASE_DATE like '2019-%' group by PRODUCTNAME, substr(PURCHASE_DATE,6,2);
 
 -- 4.count the customers from each country--
-select count(CUSTOMERNO) total_customers, COUNTRY from ECOMMERCE group by COUNTRY;
-select count(CUSTOMERNO) total_customers from ECOMMERCE group by COUNTRY;
+select count(CUSTOMERNO) as total_customers, COUNTRY from ECOMMERCE group by COUNTRY;
+select count(CUSTOMERNO) as total_customers from ECOMMERCE group by COUNTRY;
 
 -- 5.unique product names sold each year--
-select distinct(PRODUCTNAME), substr(PURCHASE_DATE,1,4) yearly_basis from ECOMMERCE group by substr(PURCHASE_DATE,1,4), PRODUCTNAME;
+select distinct(PRODUCTNAME), substr(PURCHASE_DATE,1,4) as yearly_basis from ECOMMERCE group by substr(PURCHASE_DATE,1,4), PRODUCTNAME;
 
 
                   --Additional queries--
-                           
+
 --1.Find all transactions made on a particular date--
 SELECT productname FROM ecommerce WHERE purchase_date = '2019-02-02';
 
@@ -56,5 +56,4 @@ select productname from ecommerce order by productname;
 --11. max productname--
 select max(PRODUCTNAME) from ECOMMERCE;
 
---12.display unique customerno--
-select distinct(CUSTOMERNO) from ECOMMERCE;
+select PRODUCTNAME, Country from ECOMMERCE;
